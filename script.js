b56cd88df342cc0936fc47f7665e7670579dc307
Ht <htcvhs971@gmail.com>
2023-02-07 12:30:48 +0100

    Add test username on URL form

--- script.js
+++ script.js
@@ -5,7 +5,27 @@ const content = document.getElementById('content');
 const submitBtn = document.getElementById('submit-btn');
 
 /** test F.
-rempli tout un form
-envoi
-vérifie si le retour back end bien stocké
-ICI on va juste*/
+1-rempli tout un form
+2-Envoi
+3-vérifie si le retour back end bien stocké
+ICI on va juste vérifier que le username s'affiche ds URL*/
+
+const contactFormTest = () => {
+	username.value = 'Paul';
+	title.value = 'lorem ipsum';
+	content.value = 'Esse qui qui consequat veniam elit qui do est culpa.';
+	//evite une boucle:Si form pas encore envoyé alors click pr envoi/Si il ne trouve pas ds url 1 form validé
+	if (!window.location.search) {
+		submitBtn.click();
+
+	//Const contenant l'URL
+	const queryParams = new URLSearchParams(window.location.search);
+	//Si la longueur de username est > 0 return true
+	if (queryParams.get('username').length > 0) {
+		return true;
+	}
+	return false;
+};
+
+//J'affiche le résult du test
+contactFormTest() ? console.info('Contact form test ok') : console.error('Contact form test failed');
