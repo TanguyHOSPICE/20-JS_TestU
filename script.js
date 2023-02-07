b3f3e3198a4fce4ac3ea85eda19bd162a40d2d52
Ht <htcvhs971@gmail.com>
2023-02-07 11:57:48 +0100

    Init form Test

---
+++ script.js
@@ -0,0 +1,11 @@
+const contactForm = document.getElementById('contact-form');
+const username = document.getElementById('username');
+const title = document.getElementById('title');
+const content = document.getElementById('content');
+const submitBtn = document.getElementById('submit-btn');
+
+/** test F.
+rempli tout un form
+envoi
+vérifie si le retour back end bien stocké
+ICI on va juste*/
